22d1260e94391b1dd17880fe31eeb30046468fc9
Ruben Van Dooren <ruben.vandooren@rmdy.be>
2021-03-02 14:00:26 +0100

    - fix README.md hyperlinks - add example.dart.

--- annotations/CHANGELOG.md
+++ annotations/CHANGELOG.md
@@ -1,3 +1,8 @@
+## 0.2.1
+
+* Add example
+* Fix README hyper links
+
 ## 0.2.0
 
 * Initial release
---
+++ annotations/example/example.dart
@@ -0,0 +1,12 @@
+import 'package:mockito_builder_annotations/mockito_builder_annotations.dart';
+
+abstract class ExampleUseCase {
+  int example();
+}
+
+abstract class ExampleUseCase2 {
+  void example2();
+}
+
+@GenerateMocker([ExampleUseCase, ExampleUseCase2])
+T mock<T>() => _$mock<T>();
